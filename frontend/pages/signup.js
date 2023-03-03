8c972c4356ef362ff30ed3876a8cc47fd334d535
qye <qye2022@gmail.com>
2023-03-03 16:44:33 -0700

    update sign up page

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,47 +1,123 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import FormField from '../components/common/FormField.js'
-export default class signup extends Component {
-  render() {
-    return (
-      <div className="bg-grey min-h-screen flex flex-col bg-no-repeat bg-cover">
-        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          <div className="bg-grey px-6 py-8 rounded-xl shadow-md text-black w-full ">
-            <form>
-              <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+import { useRouter } from 'next/router';
+import axios from "axios";
 
-              <FormField
-                type="username"
-                name="username"
-                placeholder="Username"
-              />
-              <FormField
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
+export default function Signup() {
+  const router = useRouter();
+  const [username, setRegisterUserName] = useState('')
+  const [email, setRegisterEmail] = useState('')
+  const [password, setRegisterPassword] = useState('')
+  const [confirmPassword, setRegisterConfirmPassword] = useState('')
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+  const userInfo = {username, email, password} // id?
+  
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  
+  const register = (event) => {
 
-              <FormField
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
+    event.preventDefault();
 
-              <FormField
-                type="confirmPassword"
-                name="confirmPassword"
-                placeholder="Confirm Password"
-              />
+    if (!username) {
+      setError('Please enter a username');
+      return;
+    }
 
-              <p className='w-full text-center pt-5'>
-              <a className="inline-block align-baseline font-bold text-sm text-secondary bg-gray-400 text-white px-8 py-1 hover:bg-gray-300" href='/login'>
-                Sign Up
-              </a>
-              </p>
+    if (!email) {
+      setError('Please enter an email');
+      return;
+    }
 
-            </form>
-          </div>
+    if (!password) {
+      setError('Please enter a password');
+      return;
+    }
+
+    if (!confirmPassword) {
+      setError('Please confirm your password');
+      return;
+    }
+
+    
+    if (password.length < 8) {
+      setError('Password length must be at least 8 characters.')
+    }
+
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError('Invalid email');
+      return
+    }
+
+    setSuccess(true);
+
+    storeUser(userInfo);
+
+    try {
+      setTimeout(() => {
+        router.push('/')
+      }, 3000);   
+    } catch (error) {
+      console.log(error);
+    } 
+  }
+  
+  // todo: store new user info
+  function storeUser(userInfo) {
+
+  }
+
+  return (
+    <div className="bg-grey min-h-screen flex flex-col bg-no-repeat bg-cover">
+      <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <div className="bg-grey px-6 py-8 rounded-xl shadow-md text-black w-full ">
+          <form>
+            <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+            {error && <div className='text-red-600 text-xs'>{error}</div>}
+            <FormField
+              type="username"
+              name="username"
+              action={e => setRegisterUserName(e.target.value)}
+              placeholder="Username"
+            />
+            <FormField
+              type="email"
+              name="email"
+              action={e => setRegisterEmail(e.target.value)}
+              placeholder="Email"
+            />
+
+            <FormField
+              type="password"
+              name="password"
+              action={e => setRegisterPassword(e.target.value)}
+              placeholder="Password"
+            />
+
+            <FormField
+              type="password"
+              name="confirmPassword"
+              action={e => setRegisterConfirmPassword(e.target.value)}
+              placeholder="Confirm Password"
+            />
+
+            <button onClick={register} className="w-full text-center mt-5 bg-gray-400 text-white px-8 py-2 hover:bg-gray-300">
+              Sign Up
+            </button>
+
+          </form>
+          {/* <h3>{success? 'Success!'}</h3> */}
+          {success && <p className='mt-3 text-center text-red-600 text-base'>Success! Redirecting to login...</p>}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
