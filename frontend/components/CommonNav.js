887f06c69754c39fcb3afcbd48819913c41e19d3
qye <qye2022@gmail.com>
2023-03-03 16:43:57 -0700

    posts pages

--- frontend/components/CommonNav.js
+++ frontend/components/CommonNav.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
-
+import Link from 'next/link'
 export default class CommonNav extends Component {
   render() {
     return (
       <div className='w-52 h-screen bg-gray-200 p-5 '>
         <div className='flex flex-col justify-start'>
           <div className='h-10 pl-1 pt-1'>
-            <img className='rounded-full h-8 w-8' src='../public/testUserImage.jpeg'/></div>
+            <img className='rounded-full h-8 w-8' src='../public/testUserImage.jpeg' /></div>
           <div className='my-4 border-b border-grey-100 pb-4'>
-            <div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
+            <Link href="/profile"><div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
               <h3>Profile</h3>
-            </div>
-            <div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
-              <h3>Public</h3>
-            </div>
-            <div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
+            </div></Link>
+            <Link href="/posts"><div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
+              <h3>Posts</h3>
+            </div></Link>
+            <Link href="/profile"><div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
               <h3>Comments</h3>
-            </div>
+            </div></Link>
 
           </div>
-          <div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
+          <Link href="/logout"><div className='flex mb-2 justify-start items-center pl-5 hover:bg-gray-400 p-2 rounded-md cursor-pointer '>
             <h3>Logout</h3>
           </div>
+          </Link>
         </div>
       </div>
     )
--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import Nav from '../components/CommonNav'
-export default class home extends Component {
-  render() {
-    return (
-      <div>
-        <Nav/>
-      </div>
-    )
-  }
+import { useRouter } from 'next/router'
+import PostsList from './posts';
+export default function home() {
+  const router = useRouter();
+  return (
+    <div className='flex flex-row'>
+      <Nav></Nav>
+      {/* <div className='ml-5 mt-5'>
+        {router.pathname === "/posts" ? (
+          <PostsList posts={posts} />
+        ) : <p>1</p>}
+      </div> */}
+    </div>
+  )
 }
-
