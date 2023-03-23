0b6e771a883df8b78ae024c2e170961f7e462c96
Marc André Haley <hmarcand@ualberta.ca>
2023-03-23 14:18:42 -0600

    added basic auth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,7 +167,7 @@ else:
     REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework_simplejwt.authentication.JWTAuthentication',
-            
+            'rest_framework.authentication.BasicAuthentication',
         ),
     }
 
