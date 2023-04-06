46e6b0c7a5705c86ccb307a17474c965ced6c5d3
Marc André Haley <hmarcand@ualberta.ca>
2023-04-06 00:17:15 -0600

    added team18 to cors whitelist

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,7 +45,9 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://cmput404-group-project.herokuapp.com', 
+    'https://cmput404-group-project.herokuapp.com',
+    'https://social-team-18.netlify.app', 
+
 ]
 
 # Database
